4118551dc119be70073e96386816febd03b8df70
yuvanreddy <raajvardhanreddy@gmail.com>
2025-09-30 20:09:24 +0530

    Fix backend configuration: use .conf file instead of .tf file

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "flask-app-terraform-state-bucket"
-    key            = "eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "flask-app-terraform-state-lock"
-  }
-}
